c379eddd36a43c2426e53d53ce5de038fb2d8f1f
theaanderschou <74912677+theaanderschou@users.noreply.github.com>
2021-12-06 16:48:10 +0100

    burger

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// BURGERMENU
+const navslide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navlinks');
+    const navLinks = document.querySelectorAll('.navlinks li');
+
+    function toggleBurger() {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    }
+
+    burger.addEventListener('click', () => {
+        toggleBurger();
+    });
+
+    nav.addEventListener('click', () => {
+        toggleBurger();
+    });
+
+}
+
+navslide();
+
+
+  
