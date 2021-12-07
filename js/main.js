3fc51b90db947ea7dbe91fe3be4c47141009b3b1
theaanderschou <74912677+theaanderschou@users.noreply.github.com>
2021-12-07 13:25:59 +0100

    burger

--- js/main.js
+++ js/main.js
@@ -7,16 +7,19 @@ const navslide = () => {
     const nav = document.querySelector('.navlinks2');
     const navLinks = document.querySelectorAll('.navlinks2 li');
 
+    const navAn = document.querySelector('.burger1');
+    const navLinksAn = document.querySelectorAll('.burger1 li');
+
     function toggleBurger() {
         //Toggle Nav
         nav.classList.toggle('nav-active');
 
         //Animate Links
-        navLinks.forEach((link, index) => {
+        navLinksAn.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = ''
             } else {
-                link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`;
+                link.style.animation = `navLinkFade 0.3s ease forwards ${index / 6 + 0.3}s`;
             }
         });
 
@@ -28,10 +31,11 @@ const navslide = () => {
         toggleBurger();
     });
 
-    nav.addEventListener('click', () => {
+    // Close on click anywhere
+/*    nav.addEventListener('click', () => {
         toggleBurger();
     });
-
+*/
 }
 
 navslide();
