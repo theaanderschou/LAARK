394a210779832adda87cddb9f1cda0a27d4fd43c
idathoet <75076677+idathoet@users.noreply.github.com>
2021-12-08 10:29:49 +0100

    Delete laark directory

--- laark/includes/laark.php
+++
@@ -1,9 +0,0 @@
-<?php
-        // start with servername, username, password, databasename
-    $dbServername = "localhost";
-    $dbUsername = "";
-    $dbPassword = "";
-    $dbName = "laark";
-
-        // variable to start connection
-    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
